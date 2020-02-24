4489524ed53a5267577f1ce1bb76c6c10970ee63
Gabriel Levesque-Lemoyne <gabriel.levesque.lemoyne@hotmail.com>
2020-02-24 10:28:14 -0500

    ajout des fichiers et branches martel, LevesqueLemoyne

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+}
+
+function add_google_fonts() {
+    wp_enqueue_style( 'google_web_fonts', get_template_directory_uri() . 'https://fonts.googleapis.com/css?family=Roboto&display=swap' );
+}
+
+add_action('wp_enqueue_style', 'add_google_fonts');
